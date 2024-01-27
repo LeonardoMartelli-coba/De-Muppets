b04442559b00844cff630874b82737298e580a86
LeonardoMartelli-coba <97674366+LeonardoMartelli-coba@users.noreply.github.com>
2024-01-27 11:32:16 +0100

    push

--- Assets/Scripts/Duck.cs
+++ Assets/Scripts/Duck.cs
@@ -2,12 +2,15 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class Duck : MonoBehaviour
 {
     public CharacterController controller;
     public float speed = 6;
     public float maxSpeed = 8;
+    public float maxSpeedNormal = 8;
+    public float maxSpeedDash = 8;
     Vector3 velocity;
     float turnSmoothVelocity;
     public float turnSmoothTime = 0.5f;
@@ -15,7 +18,7 @@ public class Duck : MonoBehaviour
     public Vector3 boxCastHalfExtend;
 
     public float gravity = -9.81f;
-    bool isGrounded;
+    private bool isGrounded;
     public Transform groundCheck;
     public float groundDistance = 0.4f;
     public LayerMask groundMask;
@@ -29,9 +32,18 @@ public class Duck : MonoBehaviour
     public float timePressEnd ;
     public float timePress ;
     public float minJumpTime ;
-
+    [FormerlySerializedAs("dashMultiplierCurver")] public AnimationCurve dashCurver;
+    public float dashForce;
+    public float timeDashEnd ;
+    public float timeDash ;
+    public float minDashTime ;
+    private bool isDashing;
+    private bool canDash;
+    public float dashDelay;
+    public float dashDelay2;
     private Rigidbody rb;
 
+    public KeyCode Dash;
     public KeyCode Jump;
     public string HorizontalAxis;
     public string VerticalAxis;
@@ -40,6 +52,7 @@ public class Duck : MonoBehaviour
     {
         yStart = transform.position.y;
         rb = GetComponent<Rigidbody>();
+        canDash = true;
     }
 
     void Update()
@@ -57,10 +70,8 @@ public class Duck : MonoBehaviour
         //rb.AddForce(velocity * Time.deltaTime);
         float horizontal = 0;
         float vertical = 0;
-        if (!isJumping && isGrounded) {
-           horizontal = Input.GetAxisRaw(HorizontalAxis);
-           vertical = Input.GetAxisRaw(VerticalAxis);
-        }
+        horizontal = Input.GetAxisRaw(HorizontalAxis);
+        vertical = Input.GetAxisRaw(VerticalAxis);
 
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
@@ -73,7 +84,9 @@ public class Duck : MonoBehaviour
 //
             //Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             //controller.Move(moveDir.normalized * (speed * Time.deltaTime));
-            rb.AddForce(moveDir.normalized * (speed * Time.deltaTime));
+            if (!isJumping && isGrounded && !isDashing) {
+                rb.AddForce(moveDir.normalized * (speed * Time.deltaTime));
+            }
 
         }
 
@@ -83,12 +96,23 @@ public class Duck : MonoBehaviour
         if (Input.GetKey(Jump)) {
             timePress += Time.deltaTime;
         }
-        if (Input.GetKeyUp(Jump) && !isJumping && isGrounded) {
+        if (Input.GetKeyUp(Jump) && !isJumping && isGrounded && !isDashing) {
             isJumping = true;
             timePress = Mathf.Clamp(timePress, 0.01f, timePressEnd);
 
         }
+        if (Input.GetKey(Dash)) {
+            timeDash += Time.deltaTime;
+            isDashing = true;
+            maxSpeed = maxSpeedDash;
+        }
+        if (Input.GetKeyUp(Dash) && !isJumping && isGrounded && canDash) {
+            canDash = false;
+            timeDash = Mathf.Clamp(timeDash, 0.01f, timeDashEnd);
+            rb.AddForce(transform.forward * (dashCurver.Evaluate(timeDash/timeDashEnd) * dashForce), ForceMode.Acceleration);
 
+            StartCoroutine(DashDelay());
+        }
         if(isJumping) {
             float j = jumpEndTime * (timePress / timePressEnd);
             j = Mathf.Clamp(j, minJumpTime, jumpEndTime);
@@ -107,7 +131,14 @@ public class Duck : MonoBehaviour
     }
 
 
-
+    IEnumerator DashDelay()
+    {
+        yield return new WaitForSeconds(dashDelay);
+        maxSpeed = maxSpeedNormal;
+        isDashing = false;
+        yield return new WaitForSeconds(dashDelay2);
+        canDash = true;
+    }
     private void OnDrawGizmos()
     {
         Gizmos.DrawCube(groundCheck.position, boxCastHalfExtend*2);
